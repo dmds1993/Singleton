763134ee53d8d24b6473cff632d53a8579edf24c
Daniel Santos <daniel.santos@xpi.com.br>
2019-10-14 02:36:53 -0300

    Singleton

---
+++ Singleton/ChocolateBoiler.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace FabricaDeChocolate
+{
+    public class ChocolateBoiler
+    {
+        public string Name { get; set; }
+        private static ChocolateBoiler Instance { get; set; }
+
+        private static readonly object chocolateBoilerLock = new object();
+
+        private bool Empty { get; set; }
+        private bool Boiled { get; set; }
+
+        public static ChocolateBoiler GetInstace()
+        {
+            if (Instance == null)
+            {
+                lock(chocolateBoilerLock)
+                {
+                    if(Instance == null)
+                    {
+                        Console.WriteLine($"Instance ==> NULL");
+                        Instance = new ChocolateBoiler();
+                    }
+                }
+            }
+
+            return Instance;
+        }
+
+        private ChocolateBoiler()
+        {
+            Empty = true;
+            Boiled = false;
+        }
+
+        public void Fill()
+        {
+            Console.WriteLine($"Fill", Instance.ToString());
+            if(!isEmpty())
+            {
+                Empty = false;
+                Boiled = false;
+            }
+        }
+
+        public void Drain()
+        {
+            Console.WriteLine($"Drain", Instance.ToString());
+
+            if (!isEmpty() && isBoiled())
+            {
+                Empty = true; 
+            }
+        }
+
+        public void Boil()
+        {
+            Console.WriteLine($"Boil", Instance.ToString());
+
+            if (!isEmpty() && isBoiled())
+            {
+                Boiled = true;
+            }
+        }
+
+        public bool isEmpty()
+        {
+            Console.WriteLine($"isEmpty", Instance.ToString());
+
+            return Empty;
+        }
+        public bool isBoiled()
+        {
+            Console.WriteLine($"isBoiled", Instance.ToString());
+
+            return Boiled;
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿using FabricaDeChocolate;
+using System;
+using System.Threading.Tasks;
+
+namespace FabricaDeChocolate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.For(0, 100000, i =>
+            {
+                ChocolateBoiler chocolateBoiler = ChocolateBoiler.GetInstace();
+            });
+        }
+    }
+}
